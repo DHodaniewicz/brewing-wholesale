0bb5665b39f56c08f84a00291495d315e3bf4bc2
DHodaniewicz <d.hodaniewicz@gmail.com>
2023-09-17 13:35:26 +0200

    first commit

---
+++ src/main/java/pl/vm/academy/brewingwholesale/BrewingWholesaleApplication.java
@@ -0,0 +1,13 @@
+package pl.vm.academy.brewingwholesale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewingWholesaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewingWholesaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/vm/academy/brewingwholesale/BrewingWholesaleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.vm.academy.brewingwholesale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewingWholesaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
